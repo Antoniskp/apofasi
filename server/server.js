72c4dc42243e70646308912e80aa4c0ffcc2ca0d
Antonis <antoniskp@gmail.com>
2025-12-25 21:30:26 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,3 +16,16 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+
+import path from "path";
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../client/dist/index.html")
+    );
+  });
+}
