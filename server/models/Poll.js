045df5c25da5e620587519629b6888161fdf116b
Antonis <antoniskp@gmail.com>
2025-12-25 14:57:39 +0200

    Create Poll.js

---
+++ server/models/Poll.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const pollSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: true
+    },
+    options: [
+      {
+        text: String,
+        votes: {
+          type: Number,
+          default: 0
+        }
+      }
+    ],
+    votedUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Poll", pollSchema);
