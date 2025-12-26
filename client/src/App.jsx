503baa1a9dd3d1dc0925f755328b5fb34c179281
Antonis <antoniskp@gmail.com>
2025-12-26 16:33:49 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
+import News from "./pages/News.jsx";
+import Polls from "./pages/Polls.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/polls" element={<Polls />} />
       </Routes>
     </BrowserRouter>
   );
