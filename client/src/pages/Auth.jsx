8090be5891530fb27dec3d41574ef47c89933e5f
Antonis <antoniskp@gmail.com>
2025-12-27 23:54:47 +0200

    Remove unused React imports

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import AuthButtons from "../components/AuthButtons.jsx";
 
 export default function Auth() {
--- client/src/pages/Mission.jsx
+++ client/src/pages/Mission.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const pillars = [
   {
     title: "Περισσότερη διαφάνεια",
